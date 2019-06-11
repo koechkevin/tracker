80ffc535f64e23725a433b4a191dc997119becb6
koechkevin <koechkevin92@gmail.com>
2019-06-11 22:29:07 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import app from './app';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.info(chalk.blue(`This is an express app port ${port}`));
